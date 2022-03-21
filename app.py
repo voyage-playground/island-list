81ce408c289fc049a79426be478bdb9a8de4cbfa
Jaden Lemmon <jaden@jadenlemmon.com>
2022-03-21 16:53:11 -0500

    adding pages

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ db = SQLAlchemy(app)
 class Island(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     location = db.Column(db.String(), nullable=False)
 
-    def __init__(self, name, description, location):
+    def __init__(self, name, slug, description, location):
         self.name = name
+        self.slug = slug
         self.description = description
         self.location = location
 
@@ -24,6 +26,11 @@ class Island(db.Model):
 def home():
     islands = Island.query.all()
     return render_template('home.j2', islands=islands)
+
+@app.route("/islands/<string:slug>")
+def get_island(slug):
+    island = Island.query.filter_by(slug=slug).first()
+    return render_template('island.j2', island=island)
 
 @app.cli.command('rollback')
 def rollback():
@@ -36,21 +43,27 @@ def migrate():
 @app.cli.command('seed')
 def seed():
    island1 = Island(name="Super Private Island",
+                        slug="super-private-island",
                         description="Once owned by a king, this beauty can now be yours.",
                         location="50.474534, -34.297446")
    island2 = Island(name="Platform in the Middle of International Waters",
+                        slug="platform-in-the-middle",
                         description="Modeled after the famous Rose Island but better.",
                         location="58.273698, 1.983231")
    island3 = Island(name="Luxury Island with Bunker",
+                        slug="luxury-island-bunker",
                         description="Survive the end of the world in style in this beautiful island.",
                         location="13.501421, -135.080818")
    island4 = Island(name="Smallest Island Ever",
+                        slug="smallest-island",
                         description="Want to own an island without breaking the bank? This is for you.",
                         location="60.233861, -91.731105")
    island5 = Island(name="Frozen Island with igloo",
+                        slug="frozen-island-with-igloo",
                         description="The current owner has already built several tiny igloos awaiting your arrival.",
                         location="78.120248, -136.333475")
    island6 = Island(name="Jurassic Island",
+                        slug="jurassic-island",
                         description="Legend has it this island is populated with mysterious mythical creatures.",
                         location="-2.258883, 65.378591")
    db.session.add(island1)
