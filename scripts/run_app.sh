a7b1753bef5eb873afe231b55b44f7f2a0635da0
Jaden Lemmon <jaden@jadenlemmon.com>
2022-03-22 20:53:22 -0500

    Commands

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -4,13 +4,13 @@ set -e
 APP_ENV="${APP_ENV:-local}"
 
 echo "🚀 Wait for DB to start"
-/usr/local/bin/dockerize -wait "tcp://$DB_HOST:5432" -timeout 60s
+dockerize -wait "tcp://$DB_HOST:5432" -timeout 60s
 
 echo "🚀 Migrate"
-python3 -m flask migrate
+python3 -m flask commands migrate
 
 echo "🚀 Seed"
-python3 -m flask seed
+python3 -m flask commands seed
 
 echo "🚀 Run"
 python3 -m flask run --host=0.0.0.0
